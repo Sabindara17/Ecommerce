38a8ea73e00e2ccc7ba756e1f3de629b40ea3ed0
Sabindara17 <sthasabin17@gmail.com>
2023-04-05 07:38:10 +0545

    First Commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="main-footer">
+    
+    <strong>Copyright &copy; 2023 Brought to You By NATH</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="container">
+      
+      <strong>Copyright &copy; 2023 Brought to You By NATH</a></strong>
+    </div>
+</footer>
